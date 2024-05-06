4611811e2ed37bbd9572eb29f15f94a7ab56d84a
rohini-hs <167188786+rohini-hs@users.noreply.github.com>
2024-05-06 12:07:33 +0530

    c_to_f calculations added

---
+++ MathScripts/c_to_f.js
@@ -0,0 +1,8 @@
+//accept temperature in celsius
+let temp_cel = 35;
+
+//conveert celsius to fahrenheit
+let c_to_f = (temp_cel * 9/5) + 32;
+
+//output
+console.log("The farenheit value of temp in calsius :", temp_cel , "is : ",c_to_f);
