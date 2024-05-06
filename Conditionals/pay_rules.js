29f1df470b1bf87093fa1fca8299ac8cf6980964
rohini-hs <167188786+rohini-hs@users.noreply.github.com>
2024-05-06 12:31:15 +0530

    added conditional statements

---
+++ Conditionals/pay_rules.js
@@ -0,0 +1,15 @@
+// calculate general pay as variable pay + hours worked
+
+let var_pay = 15.35;
+
+let Hours_worked = 40;
+
+if (Hours_worked => 40)
+{
+     Hours_worked = Hours_worked * 1.5 ;
+}
+
+let TotalPay = var_pay * Hours_worked;
+
+console.log("Total Amout is:",TotalPay);
+
