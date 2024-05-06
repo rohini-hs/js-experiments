36ae1b8c8161e478138f05423c0c32f94b3e7d5d
rohini-hs <167188786+rohini-hs@users.noreply.github.com>
2024-05-06 12:02:10 +0530

    added radius and area of a circle

---
+++ MathScripts/area_of_circle.js
@@ -0,0 +1,8 @@
+// enter the radius of the circle
+let radius = 40;
+
+//calculate the area of the circle
+let area = 22.7 * radius * radius;
+
+console.log("Area of the circle with radius: " ,radius,"is :" ,area);
+
